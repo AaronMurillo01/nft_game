a5d8238421ef90b8472fc424ce45c437df6c582d
Aaron Murillo <amurillo11@cnm.edu>
2023-06-11 23:06:58 -0600

    working on setting the web up and set up client side to begin the task.

---
+++ client/src/components/pageHOC.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import {logo, heroImg} from '../assets';
+import styles from '../styles';
+
+const pageHOC = (Component, title, description) => () => {
+    const navigate = useNavigate();
+  return (
+    <div className={styles.hocContainer}>
+        <div className={styles.hocContentBox}>
+
+        </div>
+    </div>
+  )
+}
+
+export default pageHOC
